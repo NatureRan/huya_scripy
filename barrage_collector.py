ff9305c7a24fcd321cb4f6af77f10e07479f0bfc
NatureRan <nature608@qq.com>
2021-05-30 18:13:32 +0800

    first commit

---
+++ barrage_collector.py
@@ -0,0 +1,49 @@
+#coding:utf-8
+
+from browser_base import HuYaBrowser
+from selenium.webdriver.common.by import By
+import time, atexit, logging
+
+@atexit.register
+def before_exit():
+    hu_ya_browser.close()
+
+
+if __name__ == '__main__':
+    hu_ya_browser = HuYaBrowser('ququ1');
+    hu_ya_browser.open()
+    time.sleep(5)
+    # 开始循环获取网页信息
+    i = 0
+    data_id = 0
+    while i < 20:
+        i += 1
+        time.sleep(1)
+        page_source = hu_ya_browser.get_current_page_source()
+        # print(page_source)
+        li_list = hu_ya_browser.driver.find_elements_by_xpath('//ul[@id="chat-room__list"]/li')
+        # print(len(li_list))
+
+        for li in li_list:
+            # 往下找div元素
+            try:
+                data_id_now = int(li.get_attribute('data-id'))
+                if data_id >= data_id_now or data_id_now == 0:
+                    continue
+                data_id = data_id_now
+                div = li.find_element(By.TAG_NAME, 'div')
+                print('data_id:' + str(data_id) + div.text)
+                div_attr = div.get_attribute('class')
+                print(div_attr)
+                # div往下解析每一条弹幕内容
+                if div_attr == 'tit-h-send':
+                    print('这是一条礼物')
+                elif div_attr == 'msg-normal':
+                    print('这是一条文字弹幕')
+                elif div_attr == 'msg-nobleEnter':
+                    print('这是观众进入直播间信息')
+                else:
+                    print("消息类型未知")
+            except Exception as e:
+                logging.warning("异常：" + str(e))
+            print('----------------------------------------------')
---
+++ browser_base.py
@@ -0,0 +1,29 @@
+#coding: utf-8
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import logging
+
+class HuYaBrowser(object):
+    def __init__(self, room_name):
+        self.driver = None
+        self.chrome_options = None
+        self.room_name = room_name
+        self.url = 'https://www.huya.com/' + room_name
+
+    def open(self):
+        self.chrome_options = Options()
+        self.chrome_options.add_argument('--no-sandbox')
+        self.chrome_options.add_argument('--headless')
+        self.chrome_options.add_argument('--disable-gpu')
+        self.chrome_options.add_argument('--window-size=1920,1080')
+        print('浏览器启动')
+        self.driver = webdriver.Chrome(chrome_options=self.chrome_options)
+        self.driver.get(self.url)
+
+    def close(self):
+        print('浏览器退出')
+        self.driver.quit()
+
+    def get_current_page_source(self):
+        return self.driver.page_source
